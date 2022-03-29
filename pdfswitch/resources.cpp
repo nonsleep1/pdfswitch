2d2beb774ad91a512e87e38234934721e181ae45
nonsleep1 <liucijusu@gmail.com>
2022-03-29 12:29:26 +0300

    modified:   .vs/pdfswitch/v17/Browse.VC.db 	new file:   .vs/pdfswitch/v17/fileList.bin 	new file:   pdfswitch/favicon.ico 	new file:   pdfswitch/pdfswitch.aps 	new file:   pdfswitch/pdfswitch.rc 	modified:   pdfswitch/pdfswitch.vcxproj 	modified:   pdfswitch/pdfswitch.vcxproj.filters 	new file:   pdfswitch/resource.h 	new file:   pdfswitch/resources.cpp 	new file:   pdfswitch/resources.h 	new file:   pdfswitch/resources.idl 	new file:   pdfswitch/resources.xaml

---
+++ pdfswitch/resources.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "resources.h"
+#if __has_include("resources.g.cpp")
+#include "resources.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::pdfswitch::implementation
+{
+    resources::resources()
+    {
+        InitializeComponent();
+    }
+
+    int32_t resources::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void resources::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void resources::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
---
+++ pdfswitch/resources.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "resources.g.h"
+
+namespace winrt::pdfswitch::implementation
+{
+    struct resources : resourcesT<resources>
+    {
+        resources();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::pdfswitch::factory_implementation
+{
+    struct resources : resourcesT<resources, implementation::resources>
+    {
+    };
+}
